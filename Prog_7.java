928fa2a8e9870abb8d1c9c2015c727e9e5e88560
Shobhit241 <48983232+Shobhit241@users.noreply.github.com>
2019-08-14 19:02:19 +0530

    Add files via upload

--- Prog_7.java
+++ Prog_7.java
@@ -3,25 +3,19 @@ public class Prog_7
 {
 	public static void main(String[] args) 
 	{
-		int j,count=2,result=0;
+		int j,count=2,result=0,c;
 		for(j=3;count<=10001;j+=2)
-			if(isPrime(j) && j%2!=0)
 			{
-				result=j;
-				count++;
+				c=0;
+				for(int i=3;i<=(int)Math.sqrt(j);i+=2)
+					if(j%i==0 && j!=i)
+						c++;
+				if(c==0)
+				{
+					result=j;
+					count++;
+				}
 			}
 		System.out.println("The 10001st prime is "+result);
-	}
-	
-	public static boolean isPrime(int n)
-	{
-		int c=0;
-		for(int i=1;i<=(int)Math.sqrt(n);i++)
-			if(n%i==0)
-				c++;
-		if(c>1)
-			return false;
-		else
-			return true;
 	}
 }
